031c6bf58fee7c3f14442dc819315daf35da8605
Jesper Olsson <jesper.olsson.vxo@gmail.com>
2025-04-16 13:10:31 +0200

    created basic navigation and a few screens to navigate through. added themeContext for dark/light mode and a toggle button on the settings page.

---
+++ frontend-mobile/screens/Humidity.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Humidity = () => {
+  return (
+    <View>
+      <Text>Humidity</Text>
+    </View>
+  )
+}
+
+export default Humidity
+
+const styles = StyleSheet.create({})
