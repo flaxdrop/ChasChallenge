42ebb403b13da4106599711211dc94acb47525f3
rebeccalindman <rebeccalindman@icloud.com>
2025-05-19 11:15:50 +0200

    re-added wanted files

--- backend/src/routes/public.js
+++ backend/src/routes/public.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { createUser } from "../utils/users";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
@@ -20,10 +22,39 @@ router.get("/", (req, res) => {
     <a href=api-docs>Dokumentation</a>`);
 });
 
+// About page // todo add swagger 
+//todo add DB logic
+router.get("/about", (req, res) => {
+  res.send("About page");
+});
+
 // Create user
-router.post("/users", (req, res) => {
-    res.send('User to be created here.');
-})
+router.post("/register", async (req, res) => {
+  // Create new user (unauthenticated)
+  try {
+    const { username, password, role } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).json({ error: "Username and password are required" });
+    }
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const existsCheck = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+    
+    if (existsCheck.rows.length > 0) {
+        return res.status(409).json({ error: "Username already exists." });
+      }
+
+    const user = await createUser({ username, hashedPassword, role });
+
+    res.status(201).json({ message: "User created successfully", user});
+  } catch (err) {
+    console.error("Error in /register:", err.message);
+    res.status(500).json({ error: "Server error: Failed to create user" });
+  }
+});
 
 // Login and generate JWT-token
 router.post("/login", (req, res) => {
