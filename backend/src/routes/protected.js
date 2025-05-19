7904589b248c5ff20685ca0df48baa8904b25f4b
rebeccalindman <rebeccalindman@icloud.com>
2025-05-19 10:17:14 +0200

    todo note in protected.js

--- backend/src/routes/protected.js
+++ backend/src/routes/protected.js
@@ -4,6 +4,8 @@ import sensorsRoutes from "./sensors.js";
 
 const router = express.Router();
 
+// todo add jwt check with middleware
+
 // Routes för de olika endpointsen
 router.use("/measurements", measurements); // TODO Flytta authorize till server.js.
 router.use("/sensors", sensorsRoutes);
