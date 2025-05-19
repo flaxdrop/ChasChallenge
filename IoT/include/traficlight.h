ab9b9ea151a0cb6315d6e7241093d8d8ee7e0d92
Emanuela Asiedu <emanuela.asiedu@chasacademy.se>
2025-05-19 11:46:00 +0200

    uppdated the trafic light code

--- IoT/include/traficlight.h
+++ IoT/include/traficlight.h
@@ -2,9 +2,10 @@
 #define TRAFICLIGHT_H
 #include <Arduino.h>
 
-#define MQ2_PIN A0   // MQ-2 sensor på analog pin A0
-#define RED_LED A3   // Röd LED (dålig luft/gas)
-#define GREEN_LED A1 // Grön LED (bra luft)
+#define MQ2_PIN A0    // MQ-2 sensor på analog pin A0
+#define RED_LED A3    // Röd LED (dålig luft/gas)
+#define GREEN_LED A1  // Grön LED (bra luft)
+#define YELLOW_LED A2 // Yellow LED (låg nivå dålig luft/gas)
 
 void setup()
 {
@@ -19,18 +20,30 @@ void loop()
     Serial.print("\nSensor value: ");
     Serial.print(sensorValue);
 
+    //  Farligt värde
     if (sensorValue > 450)
-    { // Om gasnivån är hög
+    {
         digitalWrite(RED_LED, HIGH);
         digitalWrite(GREEN_LED, LOW);
-        Serial.println(" | Gas detected!");
+        digitalWrite(YELLOW_LED, LOW);
+        Serial.println(" | High level gas detected!");
     }
+    // Mellan värde
+    else if (sensorValue > 378)
+    {
+        digitalWrite(YELLOW_LED, HIGH);
+        digitalWrite(RED_LED, LOW);
+        digitalWrite(GREEN_LED, LOW);
+        Serial.println(" | Low level gas detected!");
+    }
+    // Säkert värde
     else
-    { // Ingen farlig gas
+    {
         digitalWrite(RED_LED, LOW);
         digitalWrite(GREEN_LED, HIGH);
+        digitalWrite(YELLOW_LED, LOW);
+        Serial.println(" | No gas detected!");
     }
-
     delay(1000); // Vänta en sekund innan nästa mätning
 }
 
