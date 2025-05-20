25592be0b3fc7e4be6ee157dba3ac0480d4e6c59
rebeccalindman <rebeccalindman@icloud.com>
2025-05-20 17:10:39 +0200

    more specific DB query

--- backend/src/middleware/auth/validateUserLogin.js
+++ backend/src/middleware/auth/validateUserLogin.js
@@ -4,7 +4,7 @@ const validateUserLogin = async (req, res, next) => {
     const { username } = req.body;
   
     try {
-      const result = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+      const result = await pool.query("SELECT id, role, hashedpassword FROM users WHERE username = $1", [username]);
       if (result.rows.length === 0) {
         return res.status(401).json({ error: "Invalid credentials." });
       }
