133b0ff0f6c00d98a0559b91b71b79aaa6de5539
Jesper Olsson <jesper.olsson.vxo@gmail.com>
2025-05-20 13:55:45 +0200

    Switch toggle buttons for notifications and dark mode. Notification is only a placeholder. Structured settings screen a bit.

--- frontend-mobile/screens/Settings.jsx
+++ frontend-mobile/screens/Settings.jsx
@@ -19,55 +19,48 @@ const Settings = () => {
     if (newValue) {
       //Anropa shownotification
       console.log(newValue);
-
-      
     }
-  }
-
-  // showNotification - asynkron funktion
-
-  // Fallback för webbläge
-
-  // Fallback för simulator/emulator
-
-  // Kolla tillstånd för notiser - hårdkodat tillstånd
-
-  // Om tillstånd saknas - be om tillstånd
-
-  // Om tillstånd saknas - skicka varning
-
-  // Schemalägga en notis
+  };
 
   return (
     <Background>
       <ContainerGradient>
         <View>
-          <CustomSwitch isOn={isEnabled} onToggle={toggleSwitch}/>
-          {/* <Switch
-          trackColor={isEnabled ? "red" : "blue"}
-          thumbColor={isEnabled ? "blue" : "red"}
-          ios_backgroundColor={"#FFF"}
-          value={isEnabled}
-          onValueChange={toggleSwitch}
-          /> */}
-
+          <View style={styles.option}>
+            <Text style={styles.text}>Notifications:</Text>
+          <CustomSwitch
+            isOn={isEnabled}
+            onToggle={toggleSwitch}
+            onIcon={"bell"}
+            offIcon={"bell-off"}
+          />
+          
+          </View>
+<View style={styles.option}>
+  <Text style={styles.text}>Dark/Light theme:</Text>
+          <CustomSwitch
+            isOn={isDark}
+            onToggle={toggleTheme}
+            onIcon={"weather-night"}
+            offIcon={"weather-sunny"}
+          /></View>
         </View>
-         <Pressable style={styles.toggleButton}
-                    title={isDark ? "Light theme" : "Dark theme"}
-                    onPress={toggleTheme}
-                  >
-                    {isDark ? (
-                      <Text style={styles.text}>Toggle light mode:</Text>
-                    ) : (
-                      <Text style={styles.text}>Toggle dark mode:</Text>
-                    )}
-                    <MaterialCommunityIcons
-                      name={isDark ? "white-balance-sunny" : "weather-night"}
-                      color={theme.themeButton}
-                      size={50}
-                    />
-                  </Pressable>
-        
+        <Pressable
+          style={styles.toggleButton}
+          title={isDark ? "Light theme" : "Dark theme"}
+          onPress={toggleTheme}
+        >
+          {isDark ? (
+            <Text style={styles.text}>Toggle light mode:</Text>
+          ) : (
+            <Text style={styles.text}>Toggle dark mode:</Text>
+          )}
+          <MaterialCommunityIcons
+            name={isDark ? "white-balance-sunny" : "weather-night"}
+            color={theme.themeButton}
+            size={50}
+          />
+        </Pressable>
       </ContainerGradient>
     </Background>
   );
@@ -75,15 +68,23 @@ const Settings = () => {
 
 export default Settings;
 
-const createStyles = (theme) => StyleSheet.create({
-  toggleOption: {
-    flex: 1,
-    alignItems: "center"
-  },
-  toggleButton: {
-    alignItems: "center",
-  },
-  text: {
-    color: theme.textPrimary
-  }
-});
+const createStyles = (theme) =>
+  StyleSheet.create({
+    toggleOption: {
+      flex: 1,
+      alignItems: "center",
+    },
+    toggleButton: {
+      alignItems: "center",
+    },
+    text: {
+      color: theme.textPrimary,
+    },
+    option: {
+      flexDirection: "row",
+      alignItems: "center",
+      width: "90%",
+      justifyContent: "space-between",
+      padding: 10
+    }
+  });
