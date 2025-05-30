6cded6fd0d321d93b02817f5ae4c3c1a7d474c58
alexanderchasacademy <alexander.arvedson@chasacademy.se>
2025-05-30 10:33:55 +0200

    doxygen for secret.h, also removed the values from both variables in secret.h

--- IoT/include/secret.h
+++ IoT/include/secret.h
@@ -1,7 +1,17 @@
 #ifndef SECRET_H
 #define SECRET_H
 
-const char* hidden_ssid = "Chas Academy";
-const char* hidden_password = "EverythingLouderThanEverythingElse"; 
+/**
+ * @brief SSID (nätverksnamn) för det WiFi-nätverk som enheten ska ansluta till.
+ * 
+ * Används av WiFi.begin() i main.cpp. Ska hållas privat och inte laddas upp till Git.
+ */
+const char* hidden_ssid = "";
+/**
+ * @brief Lösenord för det WiFi-nätverk som enheten ska ansluta till.
+ * 
+ * Används tillsammans med SSID för att etablera WiFi-anslutning. Ska hållas hemligt.
+ */
+const char* hidden_password = ""; 
 
 #endif
