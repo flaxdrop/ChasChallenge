81ec03f137e1140790c03b6c829592183b1fcf5c
Erik Torres Puente <Erik.torres.puente@gmail.com>
2025-06-02 11:56:03 +0200

    Introduced a platform.OS to adapt structure positioning to Android devices

--- frontend-mobile/App.js
+++ frontend-mobile/App.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, Platform } from "react-native";
 import Navigation from "./navigation/Navigation";
 import { ThemeProvider } from "./theme/ThemeContext";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -7,21 +7,24 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 export default function App() {
   return (
     <GestureHandlerRootView style={styles.GestureHandler}>
-      <SafeAreaProvider style={styles.SafeArea}>
+      <SafeAreaProvider>
+        <View style={styles.fullScreen}>
         <ThemeProvider>
           <Navigation />
         </ThemeProvider>
+        </View>
       </SafeAreaProvider>
     </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
-  SafeArea: {
-    flex: 1,
-    marginTop: 0,
-  },
+
   GestureHandler:{
     flex:1,
+  },
+  fullScreen: {
+    flex: 1,
+    marginTop: Platform.OS === 'android' ? 25 : 0,
   }
 });
